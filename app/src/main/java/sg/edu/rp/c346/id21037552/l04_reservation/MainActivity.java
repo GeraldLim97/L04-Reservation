baef5b8e48e00c462be636db80e16071a575229c
GeraldLim97 <20137552@myrp.edu.sg>
2022-05-12 23:58:16 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21037552/l04_reservation/MainActivity.java
@@ -0,0 +1,17 @@
+package sg.edu.rp.c346.id21037552.l04_reservation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declaring of variables
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
